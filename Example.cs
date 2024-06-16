7466798c4d56331dbb6ed83163e805542a575cb7
Rodolfo Correia do Nascimento <64981849+RodolfoCorreiaNascimento@users.noreply.github.com>
2024-06-16 03:49:28 -0300

    Update Example.cs
    Added an IDGameObject where you can populate all ID for your objects to use.

--- Example.cs
+++ Example.cs
@@ -1,31 +1,24 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Example : MonoBehaviour
 {
     private OptimizedObjectFinder finder;
-    private int targetPrefix = 121; // Define o prefixo numérico desejado
 
     void Start()
     {
         finder = FindObjectOfType<OptimizedObjectFinder>(); // Encontra o OptimizedObjectFinder na cena
+        
+    }
 
-        if (finder != null)
+    void Update()
+    {
+        // Example:
+        GameObject obj = finder.FindObject(IDGameObject.Instance.Ball); 
+        transform.position = obj.transform.position;
+        if (obj != null)
         {
-            GameObject foundObject = finder.FindObjectByPrefix(targetPrefix); // Chama o método para encontrar o objeto com o prefixo especificado
-
-            if (foundObject != null)
-            {
-                Debug.Log("Objeto encontrado: " + foundObject.name);
-                // Aqui você pode realizar outras operações com o objeto encontrado, se necessário
-            }
-            else
-            {
-                Debug.Log("Objeto com prefixo " + targetPrefix + " não encontrado.");
-            }
-        }
-        else
-        {
-            Debug.LogError("OptimizedObjectFinder não encontrado na cena.");
+            Debug.Log($"Object Found: {obj}");
         }
     }
 }
